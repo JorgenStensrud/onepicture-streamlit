a176dc4e789bf1da5141c84878b36181c33ee1c8
jorgefs <jorgefs@student.matnat.uio.no>
2022-06-26 17:37:36 +0200

    test

--- app.py
+++ app.py
@@ -4,12 +4,9 @@ import streamlit as st
 picture = st.camera_input("Camera Input")
 
 if picture:
-    st.download_button("Download", picture)
-
-with open("flower.png", "rb") as file:
      btn = st.download_button(
              label="Download image",
-             data=file,
+             data=picture,
              file_name="flower.jpeg",
              mime="image/jpeg"
            )
