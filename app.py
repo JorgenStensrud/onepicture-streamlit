63132e1d10d2352a0e0fb3cfefde95d4e5d5adf2
jorgefs <jorgefs@student.matnat.uio.no>
2022-06-26 17:36:10 +0200

    test

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ picture = st.camera_input("Camera Input")
 
 if picture:
     st.download_button("Download", picture)
+
+with open("flower.png", "rb") as file:
+     btn = st.download_button(
+             label="Download image",
+             data=file,
+             file_name="flower.jpeg",
+             mime="image/jpeg"
+           )
