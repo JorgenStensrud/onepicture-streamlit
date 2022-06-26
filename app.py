015dd6bc252cd62a65b475126f6043309737713e
jorgefs <jorgefs@student.matnat.uio.no>
2022-06-26 23:36:45 +0200

    added found face

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+import cv2
 
 
 picture = st.camera_input("Camera Input")
@@ -16,3 +17,21 @@ if picture:
         file_name = "image.jpeg",
         mime = "image/jpeg"
     )
+
+def detect_face(img):
+    bytes_picture  = picture.getvalue()
+    cv2_img = cv2.imdecode(np.frombuffer(bytes_picture, np.uint8), cv2.IMREAD_COLOR)
+
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    gray = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    return faces
+
+if picture:
+    bytes_picture  = picture.getvalue()
+    cv2_img = cv2.imdecode(np.frombuffer(bytes_picture, np.uint8), cv2.IMREAD_COLOR)
+    faces = detect_face(picture)
+    st.write("Found {} faces!".format(len(faces)))
+    for (x,y,w,h) in faces:
+        cv2.rectangle(cv2_img, (x,y), (x+w, y+h), (255,0,0), 2)
+    st.image(cv2_img)
