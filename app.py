16cd540d5ee3ab4c0a81bea39133522602f7c477
jorgefs <jorgefs@student.matnat.uio.no>
2022-06-26 19:03:30 +0200

    test

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import streamlit as st
+import pyheif
+import cv2
+import numpy as np
 
 
 picture = st.camera_input("Camera Input")
 
+def format_picture(picture):
+    bytes_picture  = picture.getvalue()
+    # cv2_img = cv2.imdecode(np.frombuffer(bytes_picture, np.uint8), cv2.IMREAD_COLOR)
+    return bytes_picture
+
 if picture:
-     btn = st.download_button(
-             label="Download image",
-             data=picture,
-             file_name="flower.jpeg",
-             mime="image/jpeg"
-           )
+    st.download_button(
+        label = "Download",
+        data = format_picture(picture),
+        file_name = "image.jpeg",
+        mime = "image/jpeg"
+    )
