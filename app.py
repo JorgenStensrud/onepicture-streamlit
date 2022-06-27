c8d9325368de74cb149ae7300a72a26d2ba30e76
JorgenStensrud <89834487+JorgenStensrud@users.noreply.github.com>
2022-06-27 15:52:11 +0200

    test

--- app.py
+++ app.py
@@ -45,14 +45,21 @@ def detect_smile(picture):
     gray = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2GRAY)
     smiles = smile_cascade.detectMultiScale(gray, 1.3, 5)
     return smiles
-    
+
+def draw_rectange(picture, feature):
+    bytes_picture  = picture.getvalue()
+    cv2_img = cv2.imdecode(np.frombuffer(bytes_picture, np.uint8), cv2.IMREAD_COLOR)
+    for (x, y, w, h) in feature:
+        cv2.rectangle(cv2_img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    return cv2_img 
 
 if picture:
     bytes_picture  = picture.getvalue()
     cv2_img = cv2.imdecode(np.frombuffer(bytes_picture, np.uint8), cv2.IMREAD_COLOR)
+    # faces = detect_face(picture)
     faces = detect_face(picture)
-    # smile = detect_beard(picture)
     st.write("Found {} faces!".format(len(faces)))
-    for (x,y,w,h) in faces:
-        cv2.rectangle(cv2_img, (x,y), (x+w, y+h), (255,0,0), 2)
-    st.image(cv2_img)
+    st.image(draw_rectange(picture, faces), use_column_width=True)
+    # for (x,y,w,h) in smile:
+    #     cv2.rectangle(cv2_img, (x,y), (x+w, y+h), (255,0,0), 2)
+    # st.image(cv2_img)
