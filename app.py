be87a015f918d4060ec64f605c178bbf13909c36
JorgenStensrud <89834487+JorgenStensrud@users.noreply.github.com>
2022-06-27 15:36:30 +0200

    changed detect_smile

--- app.py
+++ app.py
@@ -33,17 +33,10 @@ def detect_smile(picture):
     bytes_picture  = picture.getvalue()
     cv2_img = cv2.imdecode(np.frombuffer(bytes_picture, np.uint8), cv2.IMREAD_COLOR)
 
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')
     gray = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x,y,w,h) in faces:
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = cv2_img[y:y+h, x:x+w]
-        smile = smile_cascade.detectMultiScale(roi_gray, 1.8, 22)
-        # for (sx,sy,sw,sh) in smile:
-        #     cv2.rectangle(roi_color, (sx,sy), (sx+sw, sy+sh), (0,255,0), 2)
-    return smile
+    smiles = smile_cascade.detectMultiScale(gray, 1.3, 5)
+    return smiles
 
 if picture:
     bytes_picture  = picture.getvalue()
